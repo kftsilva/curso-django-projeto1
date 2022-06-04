ffc0a937bd2e9f03307fc8f39dc78668500b2101
Kleber Tomaz <kftsilva@gmail.com>
2022-06-04 15:31:29 -0300

    Final Item 58

--- recipes/views.py
+++ recipes/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
+from utils.recipes.factory import make_recipe
+
+from .models import Recipe
 
 
-# Create your views here.
 def home(request):
-    return render(request, 'recipes/home.html', context={
-        'name': 'Master Chef',
+    recipes = Recipe.objects.all().order_by('-id')
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes': recipes,
+    })
+
+
+def category(request, category_id):
+    recipes = Recipe.objects.filter(category__id=category_id).order_by('-id')
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes': recipes,
+    })
+
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'recipe': make_recipe(),
+        'is_detail_page': True,
     })
